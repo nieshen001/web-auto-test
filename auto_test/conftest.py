e20ce049e4906d9b38c5c2a8c2d52778f304e703
nieshen001 <1726369108@qq.com>
2026-04-14 01:58:11 +0800

    Add Chrome options to fix CI: no-sandbox, headless

--- auto_test/conftest.py
+++ auto_test/conftest.py
@@ -3,6 +3,7 @@ import pytest
 import allure
 import os
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from config.settings import BASE_URL, IMPLICITLY_WAIT
 from utils.log_utils import LogUtils
 
@@ -10,8 +11,14 @@ logger = LogUtils.get_logger()
 
 @pytest.fixture(scope="function")
 def driver():
-    """每个测试用例独立启动一个浏览器实例，Selenium 自动管理 ChromeDriver"""
-    _driver = webdriver.Chrome()  # 自动下载匹配的驱动
+    """每个测试用例独立启动一个浏览器实例，使用 Selenium 自动管理驱动"""
+    chrome_options = Options()
+    chrome_options.add_argument('--no-sandbox')               # 解决 Linux 沙箱问题
+    chrome_options.add_argument('--disable-dev-shm-usage')    # 解决共享内存不足
+    chrome_options.add_argument('--headless')                 # 无头模式，CI 中推荐
+    chrome_options.add_argument('--disable-gpu')              # 禁用 GPU 加速
+    chrome_options.add_argument('--remote-debugging-port=9222')
+    _driver = webdriver.Chrome(options=chrome_options)
     _driver.implicitly_wait(IMPLICITLY_WAIT)
     _driver.maximize_window()
     _driver.get(BASE_URL)
