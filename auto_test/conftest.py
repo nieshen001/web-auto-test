b02389bc8f08b95415d8a08bf318f78354e09736
nieshen001 <1726369108@qq.com>
2026-04-14 02:10:49 +0800

    Simplify Chrome options and use setup-chrome action

--- auto_test/conftest.py
+++ auto_test/conftest.py
@@ -12,15 +12,9 @@ logger = LogUtils.get_logger()
 @pytest.fixture(scope="function")
 def driver():
     chrome_options = Options()
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--remote-debugging-port=9222')
-    chrome_options.add_argument('--disable-setuid-sandbox')
-    chrome_options.add_argument('--single-process')
-    chrome_options.add_argument('--disable-extensions')
-    chrome_options.add_argument('--disable-logging')
+    chrome_options.add_argument('--headless')      # 无头模式，CI 必需
+    chrome_options.add_argument('--no-sandbox')    # 解决 Linux 沙箱问题
+    # 其他选项不再添加，避免冲突
     _driver = webdriver.Chrome(options=chrome_options)
     _driver.implicitly_wait(IMPLICITLY_WAIT)
     _driver.maximize_window()
